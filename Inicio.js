39e5e88b0b310128f1415b278e5030a8a9f42d24
VickoFlores <82225605+VickoFlores@users.noreply.github.com>
2021-05-03 11:22:28 -0300

    Add files via upload

---
+++ Inicio.js
@@ -0,0 +1,48 @@
+function Des1B(){
+    document.getElementById('Des1').style.display= "block";
+
+}
+
+function Des1N(){
+    document.getElementById('Des1').style.display= "none";
+}
+
+function Cerr1B(){
+    document.getElementById('Cerr1').style.display= "block";
+}
+
+function Cerr1N(){
+    document.getElementById('Cerr1').style.display= "none";
+}
+
+function Des2B(){
+    document.getElementById('Des2').style.display= "block";
+}
+
+function Des2N(){
+    document.getElementById('Des2').style.display= "none";
+}
+
+function Cerr2B(){
+    document.getElementById('Cerr2').style.display= "block";
+}
+
+function Cerr2N(){
+    document.getElementById('Cerr2').style.display= "none";
+}
+
+function Des3B(){
+    document.getElementById('Des3').style.display= "block";
+}
+
+function Des3N(){
+    document.getElementById('Des3').style.display= "none";
+}
+
+function Cerr3B(){
+    document.getElementById('Cerr3').style.display= "block";
+}
+
+function Cerr3N(){
+    document.getElementById('Cerr3').style.display= "none";
+}
