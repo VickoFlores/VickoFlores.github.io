9a99ac4d6371dc33c2f74f620918f778aece4e2c
VickoFlores <82225605+VickoFlores@users.noreply.github.com>
2021-06-19 07:50:00 -0300

    Add files via upload

---
+++ Menu.js
@@ -0,0 +1,11 @@
+function MenuB(){
+    document.getElementById('MenuDes').style.display="block";
+    document.getElementById('EquisX').style.display="block";
+    document.getElementById('Barra').style.display="none";
+} 
+
+function MenuN(){
+    document.getElementById('MenuDes').style.display="none";
+    document.getElementById('Barra').style.display="block";
+    document.getElementById('EquisX').style.display="none";
+}
